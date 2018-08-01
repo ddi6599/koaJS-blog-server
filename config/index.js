8b17720a2d08db419ba44281e0efad9c710437f0
ddi <ddili6599@gmail.com>
2018-08-01 14:20:12 +0700

    config mysql

---
+++ config/index.js
@@ -0,0 +1,12 @@
+const config = {
+    // 数据库配置
+    database: {
+        DATABASE: 'blogsql',
+        USERNAME: 'root',
+        PASSWORD: '',
+        PORT: '3306',
+        HOST: 'localhost'
+    }
+}
+
+module.exports = config
