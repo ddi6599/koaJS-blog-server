80ec8e8e1d0b782a832bf974548034a4ef04b9af
ddi <ddili6599@gmail.com>
2018-08-01 15:16:19 +0700

    test is login

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const router = require('koa-router')()
 const articlesApi = require('../controllers/article')
-
+const checkUser = require('../middlewares/checkUser')
 router.get('/api/articles', articlesApi.getArticles);
 router.get('/', function (ctx, next) {
     ctx.body = '<div style="text-align: center;font-size: 30px;padding: 20px;">Hello,My mini blog</div>'
 });
-router.get('/user/:id', function (ctx, next) {
+router.get('/user/:id',checkUser.checkLogin, function (ctx, next) {
     ctx.body = {
         msg: '查询成功',
         status: 200,
